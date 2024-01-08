360296e05b49580e0b91337adf1ea789288c9077
Kimkorng Mao <giimguangmao@gmail.com>
2024-01-08 20:52:15 +0700

    Update project name

--- DjanFood/asgi.py
+++ DjanFood/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DjangoFood project.
+ASGI config for DjanFood project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoFood.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjanFood.settings')
 
 application = get_asgi_application()
