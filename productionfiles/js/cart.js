695c09f18c60b197bd653fb0b13d5abb6d066b60
Kimkorng Mao <giimguangmao@gmail.com>
2024-01-10 09:50:58 +0700

    fix error

--- productionfiles/js/cart.js
+++
@@ -1,5 +0,0 @@
-const checkoutButton = document.getElementById('checkout-button');
-
-checkoutButton.addEventListener('click', ()=>{
-    AlertMessage("Please note","This website is made for educational purpose only.")
-})
