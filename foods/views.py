ad2dca435e7dbe1d3d0fa0c6ac664bab4fc541ef
Kimkorng Mao <giimguangmao@gmail.com>
2024-01-11 12:17:08 +0700

    Update food deatils views

--- foods/views.py
+++ foods/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseNotAllowed
 from .models import *
 from django.urls import reverse
+from django.db.models import Sum
 
 # Create your views here.
 
@@ -16,10 +17,15 @@ def food_detail_view(request, food_id):
     food = Food.objects.get(id=food_id)
     try:
         cart_item = Cart.objects.get(user=request.user, food=food)  # Get the relevant cart item
-        food_in_cart_quantity = cart_item.quantity  
+        food_in_cart_quantity = cart_item.quantity
     except:
         food_in_cart_quantity = 1
-
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user)
+        cart_items_count = carts.count()
+        cart_items_count = carts.aggregate(Sum('quantity'))['quantity__sum']
+        total_prices = [cart_item.total_price() for cart_item in carts]
+        total_cart_price = sum(total_prices)
     # Add or update food cart
     if request.method == 'POST':
         if not request.user.is_authenticated:
@@ -41,6 +47,9 @@ def food_detail_view(request, food_id):
         'food': food,
         'food_in_cart_quantity': food_in_cart_quantity
     }
+    if request.user.is_authenticated:
+        context['cart_items_count'] = cart_items_count
+        context['total_cart_price'] = total_cart_price
     return render(request, 'foods/food_details.html', context)
 
 def remove_cart(request, food_id):
